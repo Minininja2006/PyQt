c94527e530ada92984ef73f681dc97aef7189970
Minininja2006 <91777612+Minininja2006@users.noreply.github.com>
2021-10-03 19:35:17 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,15 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+app = QApplication([])
+main_win = QWidget()
+main_win.setWindowTitle('Определитель победителя')
+button = QPushButton('Сгенерировать')
+text = QLabel('Нажми, чтобы узнать победителя')
+winner = QLabel('?')
+line = QVBoxLayout()
+line.addWidget(text, alignment=Qt.aligh(enter))
+line.addWidget(winner, alignment=Qt.aligh(enter))
+line.addWidget(button, alignment=Qt.aligh(enter))
+main_win.setLayout(line)
+main_win.show()
+app.exec_()
