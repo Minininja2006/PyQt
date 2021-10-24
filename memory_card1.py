dd6b6de35bbd78582f76ebbbe4a63785b9929390
Minininja2006 <91777612+Minininja2006@users.noreply.github.com>
2021-10-24 19:23:41 +0300

    Update memory_card1.py

--- memory_card1.py
+++ memory_card1.py
@@ -1,38 +1,92 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QMessageBox, QRadioButton, QGroupBox
+from PyQt5.QtWidgets import (
+        QApplication, QWidget, 
+        QHBoxLayout, QVBoxLayout, 
+        QGroupBox, QRadioButton,  
+        QPushButton, QLabel, QButtonGroup)
+ 
 app = QApplication([])
-window = QWidget()
-btn_ok = QPushButton('Ответить')
-question = QLabel('1000-7')
-RadioGroupBox = QGroupBox('Варианты оветов')
-rbtn_1 = QRadioButton('5')
-rbtn_2 = QRadioButton('993')
-rbtn_3 = QRadioButton('765')
-rbtn_4 = QRadioButton('Ты кто?')
-layout_ans1 = QHBoxLayout()
+ 
+btn_OK = QPushButton('Ответить')
+lb_Question = QLabel('Самый сложный вопрос в мире!')
+ 
+RadioGroupBox = QGroupBox("Варианты ответов")
+ 
+rbtn_1 = QRadioButton('Вариант 1')
+rbtn_2 = QRadioButton('Вариант 2')
+rbtn_3 = QRadioButton('Вариант 3')
+rbtn_4 = QRadioButton('Вариант 4')
+ 
+RadioGroup = QButtonGroup()
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+
+layout_ans1 = QHBoxLayout()   
 layout_ans2 = QVBoxLayout()
 layout_ans3 = QVBoxLayout()
 layout_ans2.addWidget(rbtn_1)
 layout_ans2.addWidget(rbtn_2)
 layout_ans3.addWidget(rbtn_3)
 layout_ans3.addWidget(rbtn_4)
+ 
 layout_ans1.addLayout(layout_ans2)
 layout_ans1.addLayout(layout_ans3)
+ 
 RadioGroupBox.setLayout(layout_ans1)
+ 
+AnsGroupBox = QGroupBox("Результат теста")
+lb_Result = QLabel('прав ты или нет?')
+lb_Correct = QLabel('ответ будет тут!')
+ 
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(layout_res)
+ 
 layout_line1 = QHBoxLayout()
 layout_line2 = QHBoxLayout()
 layout_line3 = QHBoxLayout()
-layout_line1.addWidget(question, alignment=Qt.AlignCenter)
-layout_line2.addWidget(RadioGroupBox)
+ 
+layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+layout_line2.addWidget(RadioGroupBox)   
+layout_line2.addWidget(AnsGroupBox)  
+RadioGroupBox.hide()
+ 
 layout_line3.addStretch(1)
-layout_line3.addWidget(btn_ok, stretch=3)
+layout_line3.addWidget(btn_OK, stretch=2)
 layout_line3.addStretch(1)
+ 
 layout_card = QVBoxLayout()
+ 
 layout_card.addLayout(layout_line1, stretch=2)
 layout_card.addLayout(layout_line2, stretch=8)
-layout_card.addStretch(2)
+layout_card.addStretch(1)
 layout_card.addLayout(layout_line3, stretch=1)
 layout_card.addStretch(1)
+layout_card.setSpacing(5)
+def show_result():
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_OK.setText('Следующий вопрос')
+def show_question():
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    btn_OK.setText('Ответить')
+    RadioGroup.setExclusive(False)
+    rbtn_1.setChecked(False)
+    rbtn_2.setChecked(False)
+    rbtn_3.setChecked(False)
+    rbtn_4.setChecked(False)
+    RadioGroup.setExclusive(True)
+def Test():
+    if 'Ответить' == btn_OK.text():
+        show_result()
+    else:
+        show_question()
+btn_OK.clicked.connect(Test)
+window = QWidget()
 window.setLayout(layout_card)
 window.show()
 app.exec_()
